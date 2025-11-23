34aa85bb36c9e71084afb28982cf7147624c00a4
alessandro-barate <alebarat@gmail.com>
2025-11-23 12:48:20 +0100

    Installing dependencies

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
-<script >
+<script>
+// Imports
+import HeaderComponent from './components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+import CookieBanner from './components/CookieBanner.vue';
+
 export default {
-  name: '',
-  components: {},
+  name: "App",
+
+  data() {
+    return {
+      visible: false,
+    };
+  },
+
+  components: {
+    HeaderComponent,
+    FooterComponent,
+    CookieBanner,
+  },
 };
 </script>
 
