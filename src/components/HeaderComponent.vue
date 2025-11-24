7ca267d8ddac7cb90a49c005bd66fc6b03796e81
alessandro-barate <alebarat@gmail.com>
2025-11-24 19:06:34 +0100

    Image added and styled

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -45,76 +45,81 @@ export default {
 </script>
 
 <template>
-    <header>
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <nav>
-                        <!-- Nav menu -->
-                        <ul class="nav-menu d-flex">
-                            <li>
-                              <a>
-                                <router-link :to="{ name: 'AboutUs' }" class="link">
-                                  chi siamo
-                                </router-link>
-                              </a>
-                            </li>
-                            <li>
-                              <a>
-                                <router-link :to="{ name: 'Projects' }" class="link">
-                                  progetti
-                                </router-link>
-                              </a>
-                            </li>
-                            <li>
-                              <a>
-                                <router-link :to="{ name: 'Birds' }" class="link">
-                                  gli uccelli della provincia di varese
-                                </router-link>
-                              </a>
-                            </li>
-                            <li>
-                              <a>
-                                <router-link :to="{ name: 'Conferences' }" class="link">
-                                  conferenze
-                                </router-link>
-                              </a>
-                            </li>
-                            <li>
-                              <a>
-                                <router-link :to="{ name: 'Courses' }" class="link">
-                                  corsi
-                                </router-link>
-                              </a>
-                            </li>
-                            <li>
-                              <a>
-                                <router-link :to="{ name: 'Publications' }" class="link">
-                                  pubblicazioni
-                                </router-link>
-                              </a>
-                            </li>
-                            <li>
-                              <a>
-                                <router-link :to="{ name: 'News' }" class="link">
-                                  news
-                                </router-link>
-                              </a>
-                            </li>
-                            <li>
-                              <a>
-                                <router-link :to="{ name: 'Links' }" class="link">
-                                  link utili
-                                </router-link>
-                              </a>
-                            </li>
-                        </ul>
-                        <!-- END Nav menu -->
-                    </nav>
-                </div>
+  <header>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <nav>
+          <!-- Nav menu -->
+            <ul class="nav-menu d-flex">
+              <li>
+                <a>
+                  <router-link :to="{ name: 'AboutUs' }" class="link">
+                  Chi siamo
+                  </router-link>
+                </a>
+              </li>
+              <li>
+                <a>
+                  <router-link :to="{ name: 'Projects' }" class="link">
+                  Progetti
+                  </router-link>
+                </a>
+              </li>
+              <li>
+                <a>
+                  <router-link :to="{ name: 'Birds' }" class="link">
+                  Gli uccelli della provincia di Varese
+                  </router-link>
+                </a>
+              </li>
+              <li>
+                <a>
+                  <router-link :to="{ name: 'Conferences' }" class="link">
+                  Conferenze
+                  </router-link>
+                </a>
+              </li>
+              <li>
+                <a>
+                  <router-link :to="{ name: 'Courses' }" class="link">
+                  Corsi
+                  </router-link>
+                </a>
+              </li>
+              <li>
+                <a>
+                  <router-link :to="{ name: 'Publications' }" class="link">
+                  Pubblicazioni
+                  </router-link>
+                  </a>
+              </li>
+              <li>
+                <a>
+                  <router-link :to="{ name: 'News' }" class="link">
+                  News
+                  </router-link>
+                  </a>
+              </li>
+              <li>
+                <a>
+                  <router-link :to="{ name: 'Links' }" class="link">
+                  Link utili
+                  </router-link>
+                </a>
+              </li>
+            </ul>
+            <!-- END Nav menu -->
+          </nav>
+          <div class="banner-container">
+            <div class="banner-img">
+              <img src="../assets/images/banner-with-logo.jpg" alt="">
             </div>
+          </div>
         </div>
-    </header>
+      </div>
+    </div>
+  </header>
 </template>
 
 <style scoped>
@@ -126,18 +131,34 @@ export default {
   }
 
   .container {
+    margin-bottom: 50px;
     background-color: rgba(0, 119, 255, 0.89);
   }
 
   nav {
-    width: 65%;
+    width: 70%;
     margin: 0 auto;
   }
 
   .nav-menu {
-    padding-top: 10px;
-    padding-bottom: 10px;
+    padding-top: 15px;
+    padding-bottom: 15px;
     justify-content: space-evenly;
+  }
+
+  .banner-container {
+    width: 100%;
+  }
+
+  .banner-img {
+    width: 83%;
+    margin: 0 auto;
+
+    img {
+      margin-bottom: -4px;
+      border-radius: 20px 20px 0 0 ;
+      box-shadow: 0 0 20px 5px rgba(143, 167, 199, 1);
+    }
   }
 
 </style>
