86b5d01cf00a4816e42ffec808dd178eaf4be95b
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2025-11-28 10:25:35 +0100

    Problem with server connection on windows

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+
+  // Questa configurazione per problema di progetto che non si apre nel browser
+  server: {
+    host: "127.0.0.1",
+    port: 5173,
+  },
+});
